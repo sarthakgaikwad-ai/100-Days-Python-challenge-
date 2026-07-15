bf2f649929df6272b3d7df3d74d612595a311400
Sarthak <sagaik95@gmail.com>
2026-07-15 23:15:52 +0530

    Add files via upload

---
+++ encrypt.py
@@ -0,0 +1,16 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+#sarthak +2
+def encrypt(original_text,shift_amount):
+    cipher_text=""#u
+    for letter in original_text:
+       shifted_position = alphabet.index(letter)+shift_amount#19+2=21
+       cipher_text += alphabet[shifted_position]#u
+
+    print(f"The encoded result is :{cipher_text}")
+
+encrypt(original_text=text,shift_amount=shift)
